be79de0329cb8b7b9d60c30f8340bda22fba8f5a
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-14 12:26:30 -0600

    Reconfigure `utils` module paths relative to module location.
    This seems to be required for my integration tests to run.

--- reverie/backend_server/utils.py
+++ reverie/backend_server/utils.py
@@ -3,18 +3,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+this_dir = os.path.abspath(os.path.dirname(__file__))
+environment_loc = os.path.abspath(f"{this_dir}/../../environment/")
+
 # Copy and paste your OpenAI API Key
 openai_api_key = os.environ.get('OPENAI_API_KEY')
 # Put your name
 key_owner = os.environ.get('KEY_OWNER')
 
 
-maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+maze_assets_loc = f"{environment_loc}/frontend_server/static_dirs/assets"
 env_matrix = f"{maze_assets_loc}/the_ville/matrix"
 env_visuals = f"{maze_assets_loc}/the_ville/visuals"
 
-fs_storage = "../../environment/frontend_server/storage"
-fs_temp_storage = "../../environment/frontend_server/temp_storage"
+fs_storage = f"{environment_loc}/frontend_server/storage"
+fs_temp_storage = f"{environment_loc}/frontend_server/temp_storage"
 
 collision_block_id = "32125"
 
