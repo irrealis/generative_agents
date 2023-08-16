92408365ddaaaf7fd5b4a1c5b1f1c5c927318f47
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-16 13:02:17 -0600

    Use `new_retrieve(...)` in tests.
    This removes the prototyped `retrieve(...)` function in
    `test_brainstorm__analysis.py` in favor of `new_retrieve(...)` defined
    in `persona/cognitive_modules/retrieve.py`, which has been modified to
    accept an optional weights vector used for computing retrieval scores.

--- tests/test_brainstorm__analysis.py
+++ tests/test_brainstorm__analysis.py
@@ -21,7 +21,7 @@ import utils
 import reverie
 from reverie import ReverieServer
 from persona.persona import Persona
-from persona.cognitive_modules.retrieve import extract_recency, extract_importance, extract_relevance, normalize_dict_floats, top_highest_x_values
+from persona.cognitive_modules.retrieve import extract_recency, extract_importance, extract_relevance, new_retrieve, normalize_dict_floats, top_highest_x_values
 from persona.cognitive_modules.converse import generate_summarize_ideas, generate_next_line
 
 from irrealis.generative_agents.test_tools import *
@@ -75,75 +75,18 @@ def rs():
   )
 
 
-def retrieve(persona, focal_points, n_count=30):
-  """
-  Given the current persona and focal points (focal points are events or
-  thoughts for which we are retrieving), we retrieve a set of nodes for each
-  of the focal points and return a dictionary.
-  Example input:
-    persona = <persona> object
-    focal_points = ["How are you?", "Jane is swimming in the pond"]
-  """
-  # <retrieved> is the main dictionary that we are returning
-  retrieved = dict()
-  for focal_pt in focal_points:
-    # Getting all nodes from the agent's memory (both thoughts and events) and
-    # sorting them by the datetime of creation.
-    # You could also imagine getting the raw conversation, but for now. 
-    nodes = [[i.last_accessed, i]
-              for i in persona.a_mem.seq_event + persona.a_mem.seq_thought
-              if "idle" not in i.embedding_key]
-    nodes = sorted(nodes, key=lambda x: x[0])
-    nodes = [i for created, i in nodes]
-
-    # Calculating the component dictionaries and normalizing them.
-    recency_out = extract_recency(persona, nodes)
-    recency_out = normalize_dict_floats(recency_out, 0, 1)
-    importance_out = extract_importance(persona, nodes)
-    importance_out = normalize_dict_floats(importance_out, 0, 1)
-    relevance_out = extract_relevance(persona, nodes, focal_pt)
-    relevance_out = normalize_dict_floats(relevance_out, 0, 1)
-
-    # Computing the final scores that combines the component values. 
-    # Note to self: test out different weights. [1, 1, 1] tends to work
-    # decently, but in the future, these weights should likely be learned, 
-    # perhaps through an RL-like process.
-    gw = [1, 1, 1]
-    master_out = dict()
-    for key in recency_out.keys():
-      master_out[key] = (persona.scratch.recency_w*recency_out[key]*gw[0]
-                     + persona.scratch.relevance_w*relevance_out[key]*gw[1]
-                     + persona.scratch.importance_w*importance_out[key]*gw[2])
-
-    master_out = top_highest_x_values(master_out, len(master_out.keys()))
-    for key, val in master_out.items():
-      print (persona.a_mem.id_to_node[key].embedding_key, val)
-      print (persona.scratch.recency_w*recency_out[key]*1,
-             persona.scratch.relevance_w*relevance_out[key]*1,
-             persona.scratch.importance_w*importance_out[key]*1)
-
-    # Extracting the highest x values.
-    # <master_out> has the key of node.id and value of float. Once we get the 
-    # highest x values, we want to translate the node.id into nodes and return
-    # the list of nodes.
-    master_out = top_highest_x_values(master_out, n_count)
-    master_nodes = [persona.a_mem.id_to_node[key]
-                    for key in list(master_out.keys())]
-
-    for n in master_nodes:
-      n.last_accessed = persona.scratch.curr_time
-    retrieved[focal_pt] = master_nodes
-
-  return retrieved
-
-
 def interview_persona(persona, message, curr_convo=None, interviewer=None, n_count=None):
   if curr_convo is None:
     curr_convo = []
   if interviewer is None:
     interviewer = 'Interviewer'
 
-  retrieved = retrieve(persona, [message], 30)[message]
+  retrieved = new_retrieve(
+    persona=persona,
+    focal_points=[message],
+    n_count=30,
+    weights=(1.,1.,1.),
+  )[message]
   summarized_idea = generate_summarize_ideas(persona, retrieved, message)
   curr_convo += [[interviewer, message]]
   response = generate_next_line(persona, interviewer, curr_convo, summarized_idea)
