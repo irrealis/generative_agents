4c012bcc2e06cf338929c2b821438e08c72c24a3
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-18 17:06:08 -0600

    Add prototype of ablate_observations_planning_reflection(...).

--- tests/test_brainstorm__analysis.py
+++ tests/test_brainstorm__analysis.py
@@ -94,6 +94,65 @@ def is_reflection_error(node):
   return (node.type == 'thought') and isinstance(node.filling, str)
 
 
+def ablate_observations_planning_reflection(persona):
+  initial_event_memory = persona.a_mem.seq_event[-1]
+  s = persona.name
+  p = 'is'
+  o = persona.name
+  event_description = f'{s} {p} {o}'
+  event_keywords = {s}
+  event_embedding = get_embedding(event_description)
+  event_embedding_pair = (event_description, event_embedding)
+  event_poignancy = generate_poig_score(persona, "event", event_description)
+  persona.a_mem.add_event(
+    created = initial_event_memory.created,
+    expiration = None,
+    s = s,
+    p = p,
+    o = o,
+    description = event_description,
+    keywords = event_keywords,
+    poignancy = event_poignancy,
+    embedding_pair = event_embedding_pair,
+    filling = [],
+  )
+
+  # Extract the empty memory.
+  empty_memory = persona.a_mem.seq_event[0]
+
+  # Give it bogus ID information.
+  empty_memory.node_id = 'node_0'
+  empty_memory.node_count = 0
+  empty_memory.type_count = 0
+  persona.a_mem.seq_event[0] = empty_memory
+  persona.a_mem.seq_event = [empty_memory]
+
+  # Wipe all other memories.
+  narrowed_kw_to_event = {
+    k:[node for node in nodes if node.node_id == empty_memory.node_id]
+    for k,nodes in persona.a_mem.kw_to_event.items()
+    if empty_memory.node_id in [
+      node.node_id
+      for node in nodes
+    ]
+  }
+  persona.a_mem.kw_to_event = narrowed_kw_to_event
+  persona.a_mem.id_to_node = {
+    empty_memory.node_id: empty_memory
+  }
+  persona.scratch.chatting_with = None
+  persona.scratch.chat = None
+  persona.scratch.chatting_with_buffer = []
+  persona.scratch.daily_req = []
+  persona.scratch.daily_plan_req = []
+  persona.scratch.f_daily_schedule = []
+  persona.scratch.f_daily_schedule_hourly_org = []
+  persona.a_mem.seq_thought = []
+  persona.a_mem.seq_chat = []
+
+  persona.a_mem.kw_to_thought = dict()
+  persona.a_mem.kw_to_chat = dict()
+
 
 # This brainstorm prototypes a procedure to interview a persona under ablation
 # of reflection memories.
