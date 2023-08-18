f2e27e5252e897909b6d9ef27bf8d490d497fb6c
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-18 17:15:29 -0600

    Add test_integration__ablate_planning_reflection

--- tests/test_brainstorm__analysis.py
+++ tests/test_brainstorm__analysis.py
@@ -245,6 +245,27 @@ Response:
   )
 
 
+def test_integration__ablate_planning_reflection(rs):
+  persona = rs.personas['Isabella Rodriguez']
+
+  # Sanity checks.
+  assert len(persona.a_mem.seq_thought) > 1
+
+  ablate_planning_reflection(persona)
+
+  assert len(persona.a_mem.id_to_node) > 1
+  assert len(persona.a_mem.seq_event) > 1
+  assert len(persona.a_mem.seq_chat) > 0
+  assert len(persona.a_mem.seq_thought) == 0
+  assert len(persona.a_mem.kw_to_chat) > 0
+  assert len(persona.a_mem.kw_to_thought) == 0
+  assert len(persona.scratch.chatting_with_buffer) > 0
+  assert len(persona.scratch.daily_req) == 0
+  assert len(persona.scratch.daily_plan_req) == 0
+  assert len(persona.scratch.f_daily_schedule) == 0
+  assert len(persona.scratch.f_daily_schedule_hourly_org) == 0
+
+
 # This brainstorm prototypes a procedure to interview a persona under partial
 # ablation: no planning or reflection memories.
 #
