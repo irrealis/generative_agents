efed54ec66ce4454a050b9afcbae1ab8b5ad65ee
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-21 15:34:07 -0600

    Move ReverieTestServer and rs test fixture to tests/reverie_setup.py.

---
+++ tests/reverie_setup.py
@@ -0,0 +1,34 @@
+import reverie
+
+import pytest
+
+import os, shutil
+
+
+class ReverieTestServer(reverie.ReverieServer):
+  '''
+  Version of ReverieServer for test fixtures.
+  '''
+  def __init__(self, fork_sim_code, sim_code, predelete=False):
+    '''
+    Overrides ReverieServer.__init__() to optionally predelete sim directory
+
+    Predelete is for test purposes.
+    '''
+    sim_path=f'{reverie.fs_storage}/{sim_code}'
+    if predelete and os.path.exists(sim_path):
+      shutil.rmtree(sim_path)
+    super().__init__(fork_sim_code=fork_sim_code, sim_code=sim_code)
+
+
+@pytest.fixture
+def rs():
+  fork_sim_code = 'July1_the_ville_isabella_maria_klaus-step-3-20'
+  sim_code = 'test-simulation'
+  return ReverieTestServer(
+    fork_sim_code=fork_sim_code,
+    sim_code=sim_code,
+    predelete=True,
+  )
+
+
