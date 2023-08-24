c360c13679fe65c986d2b3b0771114ec6331025b
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-24 17:28:09 -0600

    Add logger, revise import paths in associative_memory.py.

--- reverie/backend_server/persona/memory_structures/associative_memory.py
+++ reverie/backend_server/persona/memory_structures/associative_memory.py
@@ -7,8 +7,18 @@ Description: Defines the core long-term memory module for generative agents.
 Note (May 1, 2023) -- this class is the Memory Stream module in the generative
 agents paper. 
 """
-import sys
-sys.path.append('../../')
+import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+from dotenv import find_dotenv, load_dotenv
+env_path = find_dotenv()
+load_dotenv(env_path)
+
+import os, sys
+project_dir = os.path.dirname(os.path.abspath(env_path))
+sys.path.insert(0, os.path.abspath(f"{project_dir}/reverie/backend_server"))
+
 
 import json
 import datetime
