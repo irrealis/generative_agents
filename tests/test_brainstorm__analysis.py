5e5f3e63dd9b39a6e8b2e18b988ee26cbafbf145
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-24 16:55:54 -0600

    Add `test_prototype__ConceptNode__is_chat_event`.

--- tests/test_brainstorm__analysis.py
+++ tests/test_brainstorm__analysis.py
@@ -325,6 +325,17 @@ def test_prototype__ConceptNode__is_bootstrap_thought(prototype_concept_nodes):
   assert not prototype_concept_nodes['chat'].is_bootstrap_thought()
 
 
+def test_prototype__ConceptNode__is_chat_event(prototype_concept_nodes):
+  assert prototype_concept_nodes['chat_event'].is_chat_event()
+  assert not prototype_concept_nodes['object_observation_event'].is_chat_event()
+  assert not prototype_concept_nodes['activity_event'].is_chat_event()
+  assert not prototype_concept_nodes['plan_thought'].is_chat_event()
+  assert not prototype_concept_nodes['reflection_thought'].is_chat_event()
+  assert not prototype_concept_nodes['reflection_error_thought'].is_chat_event()
+  assert not prototype_concept_nodes['bootstrap_thought'].is_chat_event()
+  assert not prototype_concept_nodes['chat'].is_chat_event()
+
+
 def test_integration__believability_interviews(rs, questions):
   environment_loc = f"{project_dir}/environment"
   fs_storage = f"{environment_loc}/frontend_server/storage"
