599f2ef462b5b87bd74738c01c4763c91bfdfe37
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-24 17:44:23 -0600

    Rename to `test_unit__ConceptNode__is_reflection_thought`.

--- tests/test_brainstorm__analysis.py
+++ tests/test_brainstorm__analysis.py
@@ -278,7 +278,7 @@ def test_unit__ConceptNode__is_plan_thought(concept_nodes):
   assert not concept_nodes['chat'].is_plan_thought()
 
 
-def test_prototype__ConceptNode__is_reflection_thought(concept_nodes):
+def test_unit__ConceptNode__is_reflection_thought(concept_nodes):
   assert not concept_nodes['chat_event'].is_reflection_thought()
   assert not concept_nodes['object_observation_event'].is_reflection_thought()
   assert not concept_nodes['activity_event'].is_reflection_thought()
