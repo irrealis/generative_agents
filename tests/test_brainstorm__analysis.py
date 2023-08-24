482d41bc3128d551b7243502fda1a7ac18fb7e76
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-24 17:47:00 -0600

    Rename to `test_unit__ConceptNode__is_object_observation_event`.

--- tests/test_brainstorm__analysis.py
+++ tests/test_brainstorm__analysis.py
@@ -333,7 +333,7 @@ def test_unit__ConceptNode__is_object_observation_event(concept_nodes):
   assert not concept_nodes['chat'].is_object_observation_event()
 
 
-def test_prototype__ConceptNode__is_activity_event(concept_nodes):
+def test_unit__ConceptNode__is_activity_event(concept_nodes):
   assert not concept_nodes['chat_event'].is_activity_event()
   assert not concept_nodes['object_observation_event'].is_activity_event()
   assert concept_nodes['activity_event'].is_activity_event()
