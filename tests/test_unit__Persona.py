48b132f39cd2acc638f7ab36e7f405fcdbc48b90
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-08-29 18:59:03 -0600

    Add `test_unit__Persona__format_bootstrap_or_reflection_thought`.

--- tests/test_unit__Persona.py
+++ tests/test_unit__Persona.py
@@ -25,3 +25,10 @@ def test_unit__Persona__format_plan_thought(rs, concept_nodes):
   formatted_thought = persona.format_plan_thought(concept_nodes['plan_thought'])
   expected_formatted_thought = "- At 00:00:00: Isabella Rodriguez is thinking about what to do. This is Isabella Rodriguez's plan for Monday February 13: wake up and complete the morning routine at 6:00 am, travel to Hobbs Cafe at 7:00 am, open up shop at 8:00 am, greet customers and work at the counter until 8 pm, buy party materials for the Valentine's Day party at the cafe from 9:00 am to 10:00 am, have lunch at 12:00 pm, take a short nap from 2 to 4 pm, plan the Valentine's Day Party in the afternoon."
   assert formatted_thought == expected_formatted_thought
+
+
+def test_unit__Persona__format_bootstrap_or_reflection_thought__bootstrap(rs, concept_nodes):
+  persona = rs.personas['Isabella Rodriguez']
+  formatted_thought = persona.format_bootstrap_or_reflection_thought(concept_nodes['bootstrap_thought'])
+  expected_formatted_thought = "- At 00:00:20: Isabella Rodriguez reflects that Isabella Rodriguez is excited to be planning a Valentine's Day party at Hobbs Cafe on February 14th from 5pm and is eager to invite everyone to attend the party."
+  assert formatted_thought == expected_formatted_thought
